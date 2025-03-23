eb7afc22c1467f041e455eb58048b3eb95203ed4
Ryan <ry.allen.email@gmail.com>
2025-03-23 15:40:15 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,4 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   site: "https://ryanslabroom.github.io/AstroTemplate/", //remove later
-  base: "/github-pages", //remove later
 });
