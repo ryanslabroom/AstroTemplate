d42693ee105d5b1b5a7404ff7261b1210c5fd68d
Ryan <ry.allen.email@gmail.com>
2025-03-24 08:57:51 -0400

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#007dfc',
+          secondary: '#0056b3',
+          danger: '#E63946',
+          'danger-hover': '#C1272D',
+          'primary-hover': '#004494',
+        },
+      },
+    },
+    plugins: [],
+  }
