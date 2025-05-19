8fabc3420d769eda2b3fdd686cc00625f3dffc7a
Ryan <ry.allen.email@gmail.com>
2025-05-19 10:00:19 -0400

    update
    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
   site: "https://farrellac.com/",
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [partytown()],
 });
